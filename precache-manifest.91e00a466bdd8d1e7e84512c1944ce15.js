0c1cbc584eca4abf8312bf394cb4ef7ba369185c
josephnormandev <jnormantransactions@gmail.com>
2019-10-06 18:02:04 -0500

    Updates

---
+++ precache-manifest.91e00a466bdd8d1e7e84512c1944ce15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df104e8d9fe99bf29abebd55b967585",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fa93bb864a4a8e8bc38f",
+    "url": "/portfolio/static/css/main.a85ab013.chunk.css"
+  },
+  {
+    "revision": "7fe638cd7d73fcfc0625",
+    "url": "/portfolio/static/js/2.488ec697.chunk.js"
+  },
+  {
+    "revision": "fa93bb864a4a8e8bc38f",
+    "url": "/portfolio/static/js/main.d36500da.chunk.js"
+  },
+  {
+    "revision": "a46b3fd8f34f8691f45e",
+    "url": "/portfolio/static/js/runtime-main.6c1b9ebb.js"
+  }
+]);
